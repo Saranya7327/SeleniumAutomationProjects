ed0d91cba92ed3dbedf0bc82dd7e551ab08e669e
saranyadevi.b <saranyaraj7327@gmail.com>
2022-05-27 13:08:35 +0530

    sample proj commit

---
+++ sampleproj/src/sampleproj/sampleproj.java
@@ -0,0 +1,12 @@
+package sampleproj;
+
+public class sampleproj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
