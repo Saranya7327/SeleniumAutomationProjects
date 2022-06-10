208e0b64505acfb1357640f0794f75110742b8ca
saranyadevi.b <saranyaraj7327@gmail.com>
2022-06-10 18:55:11 +0530

    10.06.2022_ commit

--- sampleproj/src/sampleproj/test.java
+++ sampleproj/src/sampleproj/test.java
@@ -1,10 +1,11 @@
 package sampleproj;
 
-public class sample2 {
+public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world is pushed");
+
+		System.out.println("third push");
 	}
 
 }
