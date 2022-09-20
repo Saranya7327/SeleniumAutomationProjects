8154c7702e1dd3c1023251cd78a058de7c5477d4
saranyadevi.b <saranyaraj7327@gmail.com>
2022-09-20 12:24:44 +0530

    Updated with selenium

--- mavenAction/src/test/java/mavenAction/App.java
+++ mavenAction/src/test/java/mavenAction/App.java
@@ -1,11 +1,19 @@
 package mavenAction;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class App {
 	
-	 public static void main( String[] args )
+	public static void main(String[] args)
+
 	 {
-	
-	 System.out.println("ans");
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saranya\\Selenium\\chromedriver.exe");
+		 		WebDriver driver=new ChromeDriver();
+		 		driver.get("https://google.com");
 	 }
+	 
+	 
 	
 }
